28572ae721efe9524ec24c524cf587ac4ce60638
joseph bartels <joebartels@hotmail.com>
2014-10-19 06:06:53 -0400

    WIP initial commit for ember-cli-s3-sync

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+'use strict';
+
+var commands = require('./lib/commands');
+
 module.exports = {
-  name: 'ember-cli-s3'
-};
+  name: 'ember-cli-deploy',
+
+  includedCommands: function() {
+    return commands;
+  },
+
+  deployConfigs: function() {
+    this.ui.writeLine('placeholder for deploy configurations');
+    return {
+      aws: {
+        key: 'abcde',
+        secret: '123455',
+      },
+      redis: {
+        key: 'hehe',
+        port: 6742
+      }
+    };
+  }
+
+}
