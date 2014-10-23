3019e643736f8270c275ea70ba8d5fa5019a9cdb
joseph bartels <joebartels@hotmail.com>
2014-10-23 14:09:18 -0400

    allows skipping build process with --skipBuild flag. Placeholder for configurable 'between' steps

--- index.js
+++ index.js
@@ -7,20 +7,6 @@ module.exports = {
 
   includedCommands: function() {
     return commands;
-  },
-
-  deployConfigs: function() {
-    this.ui.writeLine('placeholder for deploy configurations');
-    return {
-      aws: {
-        key: 'abcde',
-        secret: '123455',
-      },
-      redis: {
-        key: 'hehe',
-        port: 6742
-      }
-    };
   }
 
 }
