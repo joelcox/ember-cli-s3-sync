7c528da6f5462c10564e0c722d94945f4f1dcef4
joseph bartels <joebartels@hotmail.com>
2015-02-08 00:21:39 -0500

    [TESTS] include some tests

--- tests/unit/commands/deploy-s3-test.js
+++ tests/unit/commands/deploy-s3-test.js
@@ -3,11 +3,6 @@
 var assert    = require('assert');
 var requiring = require('requiring');
 
-// var command        = require('../../../lib/commands/deploy-s3');
-
-// var stub           = require('../../helpers/stub').stub;
-// var commandOptions = require('../../factories/command-options');
-
 describe('deploy:s3 command with options', function() {
   var commandOptions;
   var DeployS3Command = require('../../../lib/commands/deploy-s3');
@@ -45,44 +40,4 @@ describe('deploy:s3 command with options', function() {
     DeployS3Command = null;
   });
 
-
-  it('has correct options', function() {
-    new ServeCommand(options).validateAndRun([
-      '--port', '4000'
-    ]);
-
-    var serveRun = tasks.Serve.prototype.run;
-    var ops = serveRun.calledWith[0][0];
-
-    assert.equal(serveRun.called, 1, 'expected run to be called once');
-
-    assert.equal(ops.port,           4000,      'has correct port');
-    assert.equal(ops.liveReloadPort, 35529,     'has correct liveReload port');
-  });
-
-  it('has correct liveLoadPort', function() {
-    new ServeCommand(options).validateAndRun([
-      '--live-reload-port', '4001'
-    ]);
-
-    var serveRun = tasks.Serve.prototype.run;
-    var ops = serveRun.calledWith[0][0];
-
-    assert.equal(serveRun.called, 1, 'expected run to be called once');
-
-    assert.equal(ops.liveReloadPort, 4001,     'has correct liveReload port');
-  });
-
-  it('has correct proxy', function() {
-    new ServeCommand(options).validateAndRun([
-      '--proxy', 'http://localhost:3000/'
-    ]);
-
-    var serveRun = tasks.Serve.prototype.run;
-    var ops = serveRun.calledWith[0][0];
-
-    assert.equal(serveRun.called, 1, 'expected run to be called once');
-
-    assert.equal(ops.proxy, 'http://localhost:3000/', 'has correct port');
-  });
 });
--- tests/unit/models/s3-test.js
+++ tests/unit/models/s3-test.js
@@ -1,150 +1,23 @@
 'use strict';
 
-var fs              = require('fs-extra');
-var path            = require('path');
-var Builder         = require('../../../lib/models/builder');
-var BuildCommand    = require('../../../lib/commands/build');
-var commandOptions  = require('../../factories/command-options');
-var touch           = require('../../helpers/file-utils').touch;
 var assert          = require('assert');
-var Promise         = require('../../../lib/ext/promise');
-var stub            = require('../../helpers/stub').stub;
-var MockProject     = require('../../helpers/mock-project');
-var rimraf          = Promise.denodeify(require('rimraf'));
-var tmp             = require('tmp-sync');
 
-var root            = process.cwd();
-var tmproot         = path.join(root, 'tmp');
+describe('models/s3.js', function() {
+  var S3      = require('../../../lib/models/s3');
+  var options = {
+    region: "us-east-1",
+    maxRetries: 2,
+    sslEnabled: true
+  };
 
-describe('models/builder.js', function() {
-  var builder, outputPath, tmpdir;
+  var s3 = new S3(options);
 
-  describe('copyToOutputPath', function() {
-    beforeEach(function() {
-      tmpdir  = tmp.in(tmproot);
+  it('should create instance of S3 object', function() {
 
-      builder = new Builder({
-        setupBroccoliBuilder: function() { },
-        trapSignals: function() { },
-        cleanupOnExit: function() { },
-        project: new MockProject()
-      });
-    });
+    assert.equal(s3.config.maxRetries, options.maxRetries, "s3 has correct maxRetries options");
+    assert.equal(s3.config.region, options.region, "s3 has correct region option");
+    assert.equal(s3.config.sslEnabled, options.sslEnabled, "s3 has correct sslEnabled options");
 
-    afterEach(function() {
-      return rimraf(tmproot);
-    });
-
-    it('allows for non-existent output-paths at arbitrary depth', function() {
-      builder.outputPath = path.join(tmpdir, 'some', 'path', 'that', 'does', 'not', 'exist');
-
-      return builder.copyToOutputPath('tests/fixtures/blueprints/basic_2')
-        .then(function() {
-          assert(fs.existsSync(path.join(builder.outputPath, 'files', 'foo.txt')));
-        });
-    });
   });
 
-  it('clears the outputPath when multiple files are present', function() {
-    outputPath     = 'tmp/builder-fixture/';
-    var firstFile  = outputPath + '/assets/foo-bar.js';
-    var secondFile = outputPath + '/assets/baz-bif.js';
-
-    fs.mkdirsSync(outputPath + '/assets/');
-    touch(firstFile);
-    touch(secondFile);
-
-    builder = new Builder({
-      setupBroccoliBuilder: function() { },
-      trapSignals:          function() { },
-      cleanupOnExit:        function() { },
-
-      outputPath: outputPath,
-      project: new MockProject()
-    });
-
-    return builder.clearOutputPath()
-      .then(function() {
-        assert(!fs.existsSync(firstFile));
-        assert(!fs.existsSync(secondFile));
-      });
-  });
-
-  describe('Prevent deletion of files for improper outputPath', function() {
-    var command;
-
-    before(function() {
-      command = new BuildCommand(commandOptions({
-        settings: {}
-      }));
-
-      builder = new Builder({
-        setupBroccoliBuilder: function() { },
-        trapSignals: function() { },
-        cleanupOnExit: function() { },
-        project: new MockProject()
-      });
-    });
-
-    it('when outputPath is root directory ie., `--output-path=/`', function() {
-      var outputPathArg = '--output-path=/';
-      var outputPath = command.parseArgs([outputPathArg]).options.outputPath;
-      builder.outputPath = outputPath;
-
-      return builder.clearOutputPath()
-        .catch(function(error) {
-          assert.equal(error.message, 'Using a build destination path of `' + outputPath + '` is not supported.');
-        });
-    });
-
-    it('when outputPath is project root ie., `--output-path=.`', function() {
-      var outputPathArg = '--output-path=.';
-      var outputPath = command.parseArgs([outputPathArg]).options.outputPath;
-      builder.outputPath = outputPath;
-
-      return builder.clearOutputPath()
-        .catch(function(error) {
-          assert.equal(error.message, 'Using a build destination path of `' + outputPath + '` is not supported.');
-        });
-    });
-
-    it('when outputPath is a parent directory ie., `--output-path=../../`', function() {
-      var outputPathArg = '--output-path=../../';
-      var outputPath = command.parseArgs([outputPathArg]).options.outputPath;
-      builder.outputPath = outputPath;
-
-      return builder.clearOutputPath()
-        .catch(function(error) {
-          assert.equal(error.message, 'Using a build destination path of `' + outputPath + '` is not supported.');
-        });
-    });
-  });
-
-  describe('addons', function() {
-    it('allows addons to add promises postbuild', function() {
-      var addon = {
-        name: 'TestAddon',
-        postBuild: function() { }
-      };
-      var postBuild = stub(addon, 'postBuild');
-      var results = 'build results';
-      builder = new Builder({
-        setupBroccoliBuilder: function() { },
-        trapSignals:          function() { },
-        cleanupOnExit:        function() { },
-        builder: {
-          build: function() { return Promise.resolve(results); }
-        },
-        processBuildResult: function(buildResults) { return Promise.resolve(buildResults); },
-        project: {
-          addons: [addon]
-        }
-      });
-
-      return builder.build().then(function() {
-        assert.equal(postBuild.called, 1, 'expected postBuild to be called');
-        assert.equal(postBuild.calledWith[0][0], results, 'expected postBuild to be called with the results');
-      });
-    });
-  });
 });
